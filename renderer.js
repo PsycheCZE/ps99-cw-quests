5e1a5fb5d32b126476a53e590175541a3cbe73c8
PsycheCZE <163991267+PsycheCZE@users.noreply.github.com>
2024-04-06 23:16:47 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,66 +1,176 @@
-const questMap = {
-    "27": "Ice Obby Completions",
-    "46": "Catch Fish in Advanced Fishing",
-    "34": "Use Tier 4 Potions",
-    "38": "Break Comets in Best Area",
-    "40": "Make Golden Pets from Best Egg",
-    "44": "Break Lucky Blocks in Best Area",
-    "21": "Break Breakables in Best Area",
-    "39": "Break Mini-Chests in Best Area",
-    "7": "Earn Diamonds",
-    "37": "Break Coin Jars in Best Area",
-    "43": "Break Piñatas in Best Area",
-    "42": "Hatch Rare \"??\" Pets",
-    "9": "Break Diamond Breakables",
-    "41": "Make Rainbow Pets from Best Egg",
-    "20": "Hatch Best Eggs",
-    "12": "Craft Tier 3 Potions",
-    "45": "Find Chests in Advanced Digsite"
+const fetchQuestMap = async() => {
+  try {
+    const response = await fetch("https://clan.varmi.cz/api/questmap");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const questMap = await response.json();
+    return questMap;
+  } catch (error) {
+    console.error("Chyba při načítání quest map:", error);
+  }
 }
 
-  function fetchClanPosition() {
-    fetch('https://biggamesapi.io/api/clans?page=1&pageSize=50&sort=Points&sortOrder=desc', {
-    })
-    .then(response => response.json())
-    .then(data => {
-      const clans = data.data;
-      const vlpClanIndex = clans.findIndex(clan => clan.Name === "VLP") + 1;
-      document.getElementById('clanRank').innerText = `#${vlpClanIndex}`;
-    })
-    .catch(error => console.error('Chyba při načítání clan pozice:', error));
-  }
-  function fetchData() {
-    fetch('https://biggamesapi.io/api/clan/VLP')
-      .then(response => response.json())
-      .then(data => {
-        const clanPoints = data.data.Battles.GoalBattleOne.Points;
-        document.getElementById('clanPoints').innerText = `${clanPoints}`;
-  
-        const goals = data.data.Battles.GoalBattleOne.Goals;
-        const tableBody = document.getElementById('tableBody');
-        tableBody.innerHTML = '';
-        goals.forEach(goal => {
-          const row = `<tr>
-            <td>${questMap[goal.Type] || 'Unknown Quest'}</td>
-            <td>${goal.Progress}/${goal.Amount}</td>
-            <td>${goal.Stars}</td>
-          </tr>`;
-          tableBody.innerHTML += row;
-        });
-  
-        fetchClanPosition();
-        
-        const now = new Date();
-        const formattedDate = `${now.getDate()}. ${now.getMonth() + 1}. ${now.getFullYear()} ${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
-        document.getElementById('lastUpdated').innerText = `${formattedDate}`;
-      })
-      .catch(error => console.error('Error fetching data:', error));
+const formatNumber = (num, isCWPoints = false) => {
+  const sign = num < 0 ? "-" : "";
+  const absNum = Math.abs(num);
+
+  const formatWithOptionalDecimal = (value, divider, unit) => {
+    const dividedValue = value / divider;
+    let formattedNumber;
+    if (Math.floor(dividedValue) !== dividedValue) {
+      formattedNumber = dividedValue.toFixed(2);
+    } else {
+      formattedNumber = dividedValue.toFixed(0);
+    }
+    return sign + formattedNumber + unit;
+  };
+
+  if (isCWPoints) {
+    if (absNum < 1000) return sign + absNum.toFixed(0);
+    if (absNum < 1000000) return sign + (absNum / 1000).toFixed(2) + "k";
+    if (absNum < 1000000000)
+      return formatWithOptionalDecimal(absNum, 1000000, "M");
+    if (absNum < 1000000000000)
+      return formatWithOptionalDecimal(absNum, 1000000000, "B");
+  } else {
+    if (absNum < 1000) return sign + absNum.toFixed(0);
+    if (absNum < 1000000) return sign + (absNum / 1000).toFixed(0) + "k";
+    if (absNum < 1000000000)
+      return formatWithOptionalDecimal(absNum, 1000000, "M");
+    if (absNum < 1000000000000)
+      return formatWithOptionalDecimal(absNum, 1000000000, "B");
+    return formatWithOptionalDecimal(absNum, 1000000000000, "T");
   }
 
-  function closeWindow() {
-    window.api.closeWindow();
-  }
-  
+  return sign + absNum;
+}
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userId = document.getElementById("userId").value;
+  const selectedClan = document.querySelector(
+    'input[name="clanChoice"]:checked'
+  ).value;
+  localStorage.setItem("clan", selectedClan);
+  localStorage.setItem("userId", userId);
+  document.getElementById("login").style.display = "none";
+  document.getElementById("container").style.display = "block";
   fetchData();
-  setInterval(fetchData, 60000);
-  
+});
+
+const getUserId = () => {
+  const storedId = localStorage.getItem("userId");
+  return storedId ? `u${storedId}` : "udefaultId";
+}
+
+function fetchClanPosition() {
+  fetch(
+    "https://biggamesapi.io/api/clans?page=1&pageSize=100&sort=Points&sortOrder=desc",
+    {}
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const clans = data.data;
+      const vlpClanIndex = clans.findIndex((clan) => clan.Name === "VLP") + 1;
+      document.getElementById("clanRank").innerText = ` ${vlpClanIndex}`;
+    })
+    .catch((error) => console.error("Chyba při načítání clan pozice:", error));
+}
+
+const findPlayerPosition = async() => {
+  try {
+    const selectedClan = localStorage.getItem("clan");
+    const response = await fetch(
+      `https://clan.varmi.cz/api/${selectedClan}/leaderboard`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const members = await response.json();
+
+    const userId = localStorage.getItem("userId");
+    const rbxId = parseInt(userId, 10);
+
+    const player = members.find((member) => member.robloxId === rbxId);
+
+    if (player) {
+      console.log(`Pozice hráče: ${player.position}`);
+      let position = player.position;
+      let points = player.clanPoints;
+      let bothClanPosition = player.allPosition;
+      document.getElementById("userPosition").innerText = `${position}` || 0;
+      document.getElementById("userPointsAll").innerText =
+        `/${bothClanPosition}   ||   ` || 0;
+      document.getElementById("userPoints").innerText = `${points}` || 0;
+      return player.position;
+    } else {
+      console.log("Hráč nebyl nalezen");
+      return null;
+    }
+  } catch (error) {
+    console.error("Chyba při hledání hráče: ", error);
+  }
+}
+const fetchData = async() => {
+  const questMap = await fetchQuestMap();
+  if (!questMap) {
+    console.error("Quest map nebyla načtena.");
+    return;
+  }
+  const selectedClan = localStorage.getItem("clan");
+  const userId = getUserId();
+  fetch(`https://biggamesapi.io/api/clan/${selectedClan}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const clanPoints = data.data.Battles.GoalBattleTwo.Points;
+      document.getElementById("clanPoints").innerText = `${formatNumber(
+        clanPoints
+      )}`;
+
+      const goals = data.data.Battles.GoalBattleTwo.Goals;
+      const tableBody = document.getElementById("tableBody");
+      tableBody.innerHTML = "";
+
+      goals.forEach((goal) => {
+        let userContributions = goal.Contributions[userId] || "0";
+
+        let uniqueContributorsCount =
+          Object.keys(goal.Contributions).length || 0;
+        let calc = userContributions / goal.Amount;
+        let calc2 = Math.round(calc * goal.Stars);
+
+        const row = `<tr>
+          <td>${
+            questMap[goal.Type]
+              ? questMap[goal.Type]
+              : `CHYBĚJÍCÍ QUEST: ${goal.Type}`
+          }</td>
+            <td>${goal.Progress}/${goal.Amount}</td>
+            <td>${userContributions} [${calc2} ★]</td>
+            <td>${uniqueContributorsCount}</td> 
+          </tr>`;
+        tableBody.innerHTML += row;
+      });
+
+      fetchClanPosition();
+      findPlayerPosition();
+
+      const now = new Date();
+      const formattedDate = `${now.getDate()}. ${
+        now.getMonth() + 1
+      }. ${now.getFullYear()} ${now.getHours()}:${now
+        .getMinutes()
+        .toString()
+        .padStart(2, "0")}`;
+      document.getElementById("lastUpdated").innerText = `${formattedDate}`;
+    })
+    .catch((error) => console.error("Error fetching data:", error));
+}
+
+const closeWindow = () => {
+  window.api.closeWindow();
+}
+
+fetchData();
+setInterval(fetchData, 2000);
