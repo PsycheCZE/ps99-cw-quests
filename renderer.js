752f8d3b8c81ed34042af39201819beb9014c8ab
PsycheCZE <163991267+PsycheCZE@users.noreply.github.com>
2024-04-12 08:17:03 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -52,12 +52,17 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
   const selectedClan = document.querySelector(
     'input[name="clanChoice"]:checked'
   ).value;
+
   localStorage.setItem("clan", selectedClan);
   localStorage.setItem("userId", userId);
   document.getElementById("login").style.display = "none";
   document.getElementById("container").style.display = "block";
+  
+  document.getElementById("nickName").innerText = " - @" + document.querySelector("#dtlUsersIdSuggestions option[value='"+userId+"']").innerText;
+
   fetchData();
 });
+
 
 const getUserId = () => {
   const storedId = localStorage.getItem("userId");
@@ -73,7 +78,9 @@ function fetchClanPosition() {
     .then((data) => {
       const clans = data.data;
       const vlpClanIndex = clans.findIndex((clan) => clan.Name === "VLP") + 1;
-      document.getElementById("clanRank").innerText = ` ${vlpClanIndex}`;
+      const vlpClanIndex2 = clans.findIndex((clan) => clan.Name === "VLP2") + 1;
+      document.getElementById("clanRank").innerText = ` ${vlpClanIndex}/`;
+      document.getElementById("clanRank2").innerText = `${vlpClanIndex2}`;
     })
     .catch((error) => console.error("Chyba při načítání clan pozice:", error));
 }
@@ -113,45 +120,46 @@ const findPlayerPosition = async() => {
   }
 }
 const fetchData = async() => {
+  const selectedClan = localStorage.getItem("clan");
+  const userId = getUserId();
+
+  if (selectedClan == '' || userId == 'udefaultId') {
+    return;
+  }
+
   const questMap = await fetchQuestMap();
   if (!questMap) {
     console.error("Quest map nebyla načtena.");
     return;
   }
-  const selectedClan = localStorage.getItem("clan");
-  const userId = getUserId();
+
   fetch(`https://biggamesapi.io/api/clan/${selectedClan}`)
     .then((response) => response.json())
-    .then((data) => {
+    .then(async (data) => {
       const clanPoints = data.data.Battles.GoalBattleTwo.Points;
-      document.getElementById("clanPoints").innerText = `${formatNumber(
-        clanPoints
-      )}`;
+      document.getElementById("clanPoints").innerText = `${formatNumber(clanPoints)}`;
 
       const goals = data.data.Battles.GoalBattleTwo.Goals;
       const tableBody = document.getElementById("tableBody");
       tableBody.innerHTML = "";
 
-      goals.forEach((goal) => {
+      for (const goal of goals) {
         let userContributions = goal.Contributions[userId] || "0";
 
-        let uniqueContributorsCount =
-          Object.keys(goal.Contributions).length || 0;
+        let uniqueContributorsCount = Object.keys(goal.Contributions).length || 0;
         let calc = userContributions / goal.Amount;
         let calc2 = Math.round(calc * goal.Stars);
 
-        const row = `<tr>
-          <td>${
-            questMap[goal.Type]
-              ? questMap[goal.Type]
-              : `CHYBĚJÍCÍ QUEST: ${goal.Type}`
-          }</td>
-            <td>${goal.Progress}/${goal.Amount}</td>
-            <td>${userContributions} [${calc2} ★]</td>
-            <td>${uniqueContributorsCount}</td> 
-          </tr>`;
-        tableBody.innerHTML += row;
-      });
+        const row = document.createElement('tr');
+        row.innerHTML = `
+          <td>${questMap[goal.Type] ? questMap[goal.Type] : `CHYBĚJÍCÍ QUEST: ${goal.Type}`}</td>
+          <td>${goal.Progress}/${goal.Amount}</td>
+          <td>${userContributions} [${calc2} ★]</td>
+          <td>${uniqueContributorsCount}</td>
+        `;
+        row.onclick = () => showPlayerDetails(goal.Contributions);
+        tableBody.appendChild(row);
+      }
 
       fetchClanPosition();
       findPlayerPosition();
@@ -159,18 +167,114 @@ const fetchData = async() => {
       const now = new Date();
       const formattedDate = `${now.getDate()}. ${
         now.getMonth() + 1
-      }. ${now.getFullYear()} ${now.getHours()}:${now
-        .getMinutes()
-        .toString()
-        .padStart(2, "0")}`;
+      }. ${now.getFullYear()} ${now.getHours()}:${now.getMinutes().toString().padStart(2, "0")}:${now.getSeconds().toString().padStart(2, "0")}`;
       document.getElementById("lastUpdated").innerText = `${formattedDate}`;
     })
     .catch((error) => console.error("Error fetching data:", error));
 }
 
+async function showPlayerDetails(contributions) {
+  const mainContent = document.getElementById('container');
+  const detailContent = document.createElement('div');
+  detailContent.id = 'playerDetails';
+
+  const backButton = document.createElement('button');
+  backButton.id = 'backButton';
+  backButton.textContent = 'Zpět';
+  backButton.onclick = () => {
+    detailContent.remove();
+    mainContent.style.display = 'block';
+  };
+
+  const playerList = document.createElement('ul');
+
+  try {
+    const allPlayers = await fetch('https://clan.varmi.cz/api/all/leaderboard').then(res => res.json());
+    Object.entries(contributions).forEach(([userId, contribution]) => {
+      const player = allPlayers.find(player => `u${player.robloxId}` === userId);
+      const nick = player ? player.nick : 'Neznámý hráč';
+      const li = document.createElement('li');
+      li.innerHTML = `<span class="nickname">${nick}</span> - [Progress: ${contribution}]`;
+      playerList.appendChild(li);
+    });
+  } catch (error) {
+    console.error("Error loading player details: ", error);
+    const li = document.createElement('li');
+    li.textContent = 'Nepodařilo se načíst detaily hráčů.';
+    playerList.appendChild(li);
+  }
+
+  detailContent.appendChild(backButton);
+  detailContent.appendChild(playerList);
+
+  mainContent.parentNode.insertBefore(detailContent, mainContent);
+  mainContent.style.display = 'none';
+}
+
+
+const updateUsersSelectBox  = async() => {
+  console.log('updateUsersSelectBox');
+  
+  try {
+    const selectedClan = document.querySelector(
+      'input[name="clanChoice"]:checked'
+    ).value;
+
+    const response = await fetch(
+      `https://clan.varmi.cz/api/${selectedClan}/leaderboard`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    let members = await response.json();
+
+    let dtlUsersId = document.getElementById("dtlUsersIdSuggestions");
+    let userId = document.getElementById("userId");
+
+    dtlUsersId.innerHTML = '';
+    userId.value = '';
+
+    members = members.sort((a, b) => {
+      if (a.nick < b.nick) {
+        return -1;
+      }
+    });
+
+    for (let key in members) {
+      let option = document.createElement("option");
+
+      option.innerText = members[key]['nick'];
+      option.value = members[key]['robloxId'];
+
+      dtlUsersId.append(option);
+    }
+  } catch (error) {
+    console.error("Chyba při aktualizaci seznamu hráčů: ", error);
+  }
+}
+
+document.body.addEventListener('click', function(e) {
+  var target = e.target;
+  
+  if (target.nodeName.toUpperCase() === 'INPUT' && target.type.toUpperCase() == 'RADIO') {
+    localStorage.setItem("clan", target.value);
+
+    updateUsersSelectBox();
+  }
+  
+  e.stopPropagation()
+});
+
 const closeWindow = () => {
   window.api.closeWindow();
 }
 
-fetchData();
-setInterval(fetchData, 2000);
+document.addEventListener("DOMContentLoaded", (event) => {
+    localStorage.clear();
+    
+    updateUsersSelectBox(); 
+
+    setInterval(fetchData, 2000);
+})
